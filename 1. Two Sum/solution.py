a5062d655e5b6fa7af87a6eee4e798d0548151d4
The Peeps191 <74725787+ThePeeps191@users.noreply.github.com>
2021-01-13 10:12:23 -0700

    Added Solution.twoSum

--- 1. Two Sum/solution.py
+++ 1. Two Sum/solution.py
@@ -1,1 +1,7 @@
-
+class Solution:
+	def twoSum(self, nums, target):
+		for i in range(len(nums)):
+			for j in range(len(nums)):
+				if ((nums[i] + nums[j]) == target) and (i != j):
+					r = [j, i]
+		return r
